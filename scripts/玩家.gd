b534055f150e806294831e678f8f53884e2d5c82
李子峰 <2730253153@qq.com>
2026-07-19 23:59:17 +0800

    7月19日，第三版，添加了第二层草地，并且添加了走楼梯的代码。多边形需要替换。

--- scripts/玩家.gd
+++ scripts/玩家.gd
@@ -6,6 +6,13 @@ const FRICTION := 1000.0
 
 @onready var _sprite: AnimatedSprite2D = $玩家贴图
 @onready var _camera: Camera2D = $摄像机
+@onready var _area: CollisionShape2D = $"../楼梯/上碰撞"
+@onready var _monitor_up : Area2D = $"../检测上楼"
+@onready var _monitor_down : Area2D = $"../检测下楼"
+@onready var _louti : CollisionShape2D = $"../../第二层地图/第二层草地碰撞箱/楼梯上边竖条"
+
+var is_up = -1 # 玩家是否触碰楼梯上楼
+var is_stair = 1 #检测玩家是否处于上楼梯阶段
 
 func _ready() -> void:
 	_camera.position_smoothing_enabled = true
@@ -13,6 +20,13 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("左", "右", "上", "下")
+	
+		# === 新增：楼梯对角线移动 ===
+	if is_stair > 0 and direction != Vector2.ZERO:
+		direction.y -= direction.x          # 右移附带向上，左移附带向下
+		direction = direction.normalized()  # 保持单位向量
+	# ==========================
+	
 	var target_velocity := direction * SPEED
 	
 	if direction != Vector2.ZERO:
@@ -36,3 +50,24 @@ func _update_animation(direction: Vector2) -> void:
 
 	if _sprite.animation != anim_name:
 		_sprite.play(anim_name)
+	
+	
+func _on_up_body_entered(body: Node2D) -> void:
+	is_up = -1 * is_up
+	is_stair = -1 * is_stair
+	
+	if is_up > 0:
+		_area.rotation = -deg_to_rad(45)
+		z_index = 15
+		_louti.shape.b = Vector2(0.0,160.0)
+	else:
+		_area.rotation = -deg_to_rad(0)
+		z_index = 5
+		_louti.shape.b = Vector2(0.0,190.0)
+
+func _on_up_body_exited(body: Node2D) -> void:
+	pass
+
+
+func _on_down_body_entered(body: Node2D) -> void:
+	is_stair = -1 * is_stair
